94ee737dd56b7d1a0651f21260a211f4c81a4535
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-08 09:59:22 -0300

    finalização da classe pessoa

--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -1,5 +1,20 @@
 package entidades;
 
 public class Pessoa {
+	
+	protected String nome;
+	protected Long id;
+	
+	
+	public Pessoa(String nome, Long id) {
+		super();
+		this.nome = nome;
+		this.id = id;
+	}
 
+
+	public Pessoa() {
+		
+	}
+	
 }
