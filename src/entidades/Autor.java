5d78af3c15c6fdeb2af5eb5f08b6ae69c0613b3e
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-08 10:28:35 -0300

    correção de bug na classe autor finalizado

--- src/entidades/Autor.java
+++ src/entidades/Autor.java
@@ -1,70 +1,59 @@
 package entidades;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
-public class Autor extends Pessoa {
+public class Autor {
+    private Long id;
+    private String nome;
+    private String biografia;
+    private String nacionalidade;
+    private LocalDate dataNascimento;
 
-	private String biografia;
-	private String nacionalidade;
-	private LocalDate dataNascimento;
+    public Long getId() {
+        return this.id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getNome() {
+        return this.nome;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getBiografia() {
+        return this.biografia;
+    }
 
-	public String getBiografia() {
-		return biografia;
-	}
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
 
-	public void setBiografia(String biografia) {
-		this.biografia = biografia;
-	}
+    public String getNacionalidade() {
+        return this.nacionalidade;
+    }
 
-	public String getNacionalidade() {
-		return nacionalidade;
-	}
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
 
-	public void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
-	}
+    public LocalDate getDataNascimento() {
+        return this.dataNascimento;
+    }
 
-	public LocalDate getDataNascimento() {
-		return dataNascimento;
-	}
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
-	public void setDataNascimento(LocalDate dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-
-	public Autor(Long id, String nome, String biografia, String nacionalidade, LocalDate dataNascimento) {
-		super(nome, id);
-		this.biografia = biografia;
-		this.nacionalidade = nacionalidade;
-		this.dataNascimento = dataNascimento;
-	}
-
-	public Autor() {
-	}
-
-	public String getNacionalidade1() {
-		return null;
-	}
-
-	public void setDataNascimento(Date date) {
-		// TODO Auto-generated method stub
-		
-	}
+    public Autor(Long id, String nome, String biografia, String nacionalidade, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.biografia = biografia;
+        this.nacionalidade = nacionalidade;
+        this.dataNascimento = dataNascimento;
+    }
 }
