4c92dea461eb20a6b97e61671e9aa010d21f9cbd
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-08 10:01:39 -0300

    finalização da classe biblioteca

--- src/entidades/Biblioteca.java
+++ src/entidades/Biblioteca.java
@@ -1,5 +1,35 @@
 package entidades;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private List<Livro> livros;
+    private List<Usuario> usuarios;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void removerLivro(Livro livro) {
+        livros.remove(livro);
+    }
+
+    public List<Livro> listarLivros() {
+        return livros;
+    }
+
+    public void registrarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return usuarios;
+    }
 }
+
