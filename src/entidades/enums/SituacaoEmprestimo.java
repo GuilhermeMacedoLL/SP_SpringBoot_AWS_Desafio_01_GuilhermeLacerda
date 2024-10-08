baf83883610b9b3ddb566640bbfafc42ed28b534
Guilherme Macedo <macedo.lac157@gmail.com>
2024-10-08 10:04:55 -0300

    finalização da classe situação emprestimo

--- src/entidades/enums/SituacaoEmprestimo.java
+++ src/entidades/enums/SituacaoEmprestimo.java
@@ -1,5 +1,22 @@
 package entidades.enums;
 
 public enum SituacaoEmprestimo {
+	ATIVO(1), 
+	CONCLUIDO(2), 
+	ATRASADO(3);
 
+	private int codigo;
+
+	SituacaoEmprestimo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public static SituacaoEmprestimo fromCodigo(int codigo) {
+		for (SituacaoEmprestimo situacao : SituacaoEmprestimo.values()) {
+			if (situacao.codigo == codigo) {
+				return situacao;
+			}
+		}
+		throw new IllegalArgumentException("Código de situação inválido");
+	}
 }
